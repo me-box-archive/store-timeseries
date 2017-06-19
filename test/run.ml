e5894e7f9a9b7cd88ae4a750ab17e1d7ddb5eede
John Moore <jmoore@zedstar.org>
2017-06-19 10:27:39 +0100

    kv tests

--- test/run.ml
+++ test/run.ml
@@ -16,19 +16,34 @@ let post path data =
   >>= fun (_, body) ->
   body |> Cohttp_lwt_body.to_string
             
-module To_test = struct
+module Test_kv = struct
+  (* funcs *)
   let post_kv path data = Lwt_main.run (post path data)
+  let get_kv path = Lwt_main.run (get path)
+  (* some values *)
+  let key = "/foo/kv"
+  let value = "{\"bar\":42}"
+  let status_true = "{\"status\":true}"
+  let key_not_found = "/bar/kv"
+  let value_not_found = "{}"
 end
 
 let post_kv () =
-  Alcotest.(check string) "status true" "{\"status\":true}" (To_test.post_kv "/foo/kv" "{\"bar\":42}")
+  Alcotest.(check string) "status true" Test_kv.status_true (Test_kv.post_kv Test_kv.key Test_kv.value)
 
-let test_set = [
+let get_kv () =
+  Alcotest.(check string) "match value" Test_kv.value (Test_kv.get_kv Test_kv.key)
+
+let get_kv_not_found () =
+  Alcotest.(check string) "match empty" Test_kv.value_not_found (Test_kv.get_kv Test_kv.key_not_found)
+
+let test_kv = [
   "post kv" , `Quick, post_kv;
+  "get kv" , `Quick, get_kv;
+  "get kv not found", `Quick, get_kv_not_found;
 ]
 
 let () =
-  Alcotest.run "My first test" [
-    "test_set", test_set;
+  Alcotest.run "Data store" [
+    "test_kv", test_kv;
   ]
-    
